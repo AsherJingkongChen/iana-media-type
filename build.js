a8e44f2d5804a2471bf2c42807b1aacc6f2a44f9
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-01-23 13:10:17 +0800

    (fix): Wrong tree level

--- build.js
+++ build.js
@@ -6,10 +6,11 @@ fetch('https://www.iana.org/assignments/media-types/media-types.xml')
     const { document } =
       typeof process === 'undefined' ? window : new JSDOM(data).window;
 
+    const body = document.getElementById('media-types');
     const mediaTypes = Array.from(
       new Set(
         (function* () {
-          for (const registry of document.getElementsByTagName('registry')) {
+          for (const registry of body.getElementsByTagName('registry')) {
             const title = registry.querySelector('title')?.textContent;
             if (title) {
               for (const record of registry.getElementsByTagName('record')) {
